4df0e2527764caa696d3239b25290c6ff55b394b
HyounJinDev <hyounjin.dev@gmail.com>
2025-04-03 16:35:47 +0900

    [Add] 기본 PlayerController 구현 중, EnhancedInput을 사용하기 위한 모듈 추가

---
+++ Source/TheSeven/PlayerController/DefaultPlayerController.h
@@ -0,0 +1,61 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DefaultPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+/**
+ * 
+ */
+UCLASS()
+class THESEVEN_API ADefaultPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ADefaultPlayerController();
+
+#pragma region InputMapping Context
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+#pragma endregion InputMapping Context
+	
+#pragma region InputActions
+public:
+	// IA_Move
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	// IA_BasicAttack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> BasicAttackAction;
+
+	// IA_SpecialAttack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> SpecialAttackAction;
+
+	// IA_Jump
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	// IA_Guard
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> GuardAction;
+#pragma endregion InputActions
+
+#pragma region Overrided Functions
+public:
+	virtual void BeginPlay() override;
+#pragma endregion Overrided Functions
+
+#pragma region Setup Functions
+protected:
+	virtual void SetupInputMappingContext();
+#pragma endregion Setup Functions
+};
--- Source/TheSeven/TheSeven.Build.cs
+++ Source/TheSeven/TheSeven.Build.cs
@@ -8,9 +8,8 @@ public class TheSeven : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
