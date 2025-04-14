d3a67cb5609666e866313d12e735b849d81f6b7e
Wonjihui <jt3624@naver.com>
2025-04-14 12:45:21 +0900

    [Update] Build.cs에 UMG 추가

--- Source/TheSeven/TheSeven.Build.cs
+++ Source/TheSeven/TheSeven.Build.cs
@@ -8,7 +8,7 @@ public class TheSeven : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG"});
 		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
 
 		// Uncomment if you are using Slate UI
